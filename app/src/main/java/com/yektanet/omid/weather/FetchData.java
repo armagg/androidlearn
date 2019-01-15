372998e27552709b28a98cb1371e9bc27fff7629
omid <omid.mesgarha@gmail.com>
2019-01-15 14:44:01 +0330

    code clean

--- app/src/main/java/com/yektanet/omid/weather/FetchData.java
+++ app/src/main/java/com/yektanet/omid/weather/FetchData.java
@@ -6,7 +6,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -14,37 +13,47 @@ import javax.net.ssl.HttpsURLConnection;
 class FetchData extends AsyncTask<Void, Void, Void> {
     private String address;
     private String fetchedData = "";
+    private City city;
+    private JsonHandler jsonHandler;
 
-    FetchData(String address)  {
-        this.address= address;
-
+    public FetchData(String address, City city) {
+        this.address = address;
+        this.city = city;
     }
+
     @Override
     protected Void doInBackground(Void... voids) {
-
-
         try {
             URL url = new URL(address);
             HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
             InputStream input = httpsURLConnection.getInputStream();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(input));
             String line = "";
-            while (line !=null){
+            while (line != null) {
                 line = bufferedReader.readLine();
-                fetchedData +=line;
+                fetchedData += line;
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         return null;
     }
 
     @Override
-    protected void onPostExecute(Void avoid){
+    protected void onPostExecute(Void avoid) {
         super.onPostExecute(avoid);
+        jsonHandler = new JsonHandler(getFetchedData(), city);
+
     }
 
     String getFetchedData() {
         return fetchedData;
     }
+
+    public JsonHandler getJsonHandler() {
+        return jsonHandler;
+    }
+
+
 }
--- app/src/main/java/com/yektanet/omid/weather/MainActivity.java
+++ app/src/main/java/com/yektanet/omid/weather/MainActivity.java
@@ -1,76 +1,19 @@
 package com.yektanet.omid.weather;
 
-import android.annotation.SuppressLint;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
-
-import java.sql.Time;
 
 public class MainActivity extends AppCompatActivity {
-
-    JsonHandler jsonHandler;
-
-    final static String CONNECTIVITY_ACTION = "android.net.conn.CONNECTIVITY_CHANGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = findViewById(R.id.text);
-/*
-        String s = "{\"coord\":{\"lon\":51.42,\"lat\":35.69},\"weather\":[{\"id\":741,\"main\":\"Fog\"" +
-                ",\"description\":\"fog\",\"icon\":\"50d\"},{\"id\":701,\"main\":\"Mist\",\"description\":" +
-                "\"mist\",\"icon\":\"50d\"}],\"base\":\"stations\",\"main\":{\"temp\":275.08,\"pressure\"" +
-                ":1015,\"humidity\":91,\"temp_min\":273.15,\"temp_max\":277.15},\"visibility\":10000,\"wind\":" +
-                "{\"speed\":2.1,\"deg\":120},\"clouds\":{\"all\":90},\"dt\":1547445600,\"sys\":{\"type\":1,\"id\"" +
-                ":7464,\"message\":0.002,\"country\":\"IR\",\"sunrise\":1547437426,\"sunset\":1547473387},\"id\":112931,\"" +
-                "name\":\"Tehran\",\"cod\":200}";
-
-            new JsonHandler(s, City.TEHRAN);
-*/
-        String s = null;
-        JsonHandler jsonHandler;
-        /*try {
-            Network network = new Network(City.TEHRAN);
-            jsonHandler = network.getJsonHandler();
-            s = jsonHandler.getJson();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        if (s != null)
-        textView.setText(s);
-*/
-        boolean isFetched=false;
-        FetchData fetchData=null;
-        try {
-            fetchData = new FetchData(Contants.getAddress(City.TEHRAN));
-            fetchData.execute();
-            fetchData.doInBackground();
-            isFetched =true;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        assert fetchData != null;
-        String str = fetchData.getFetchedData();
-
-        if (isFetched && fetchData!=null){
-            textView.setText(str);
-        }
-
-
-
 
     }
+
 
     private Intent intentCreator(JsonHandler j) {
         Intent intent = new Intent(this, oneCityViewer.class);
@@ -88,26 +31,6 @@ public class MainActivity extends AppCompatActivity {
         }
         return intent;
     }
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        //registerReceiver(receiver, intentFilter);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-
-
-
-    @SuppressLint("ShowToast")
-    public void onClick(View view) {
-    }
-
 
     private PendingIntent creatPIntent() {
         Intent intent = new Intent(this, MainActivity.class);
