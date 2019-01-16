42db90e10a798b9166a1c0f8a918c6c2b59d2a1e
omid <omid.mesgarha@gmail.com>
2019-01-16 15:10:21 +0330

    first release

--- app/src/main/java/com/yektanet/omid/weather/NetworkChangeReceiver.java
+++ app/src/main/java/com/yektanet/omid/weather/NetworkChangeReceiver.java
@@ -1,11 +1,13 @@
 package com.yektanet.omid.weather;
 
 import android.annotation.SuppressLint;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -13,6 +15,8 @@ public class NetworkChangeReceiver extends BroadcastReceiver {
     Intent intent;
     final static String CONNECTIVITY_ACTION = "android.net.conn.CONNECTIVITY_CHANGE";
     Context context;
+    String mainFeel;
+    float temp;
     @Override
     public void onReceive(final Context context, final Intent intent) {
         System.out.println("vared shod ");
@@ -46,7 +50,32 @@ public class NetworkChangeReceiver extends BroadcastReceiver {
     void loadData() {
         Log.d("my load data ", "flag 1021");
 
-    }
+        final NotificationCompat.Builder notifBuilder = new NotificationCompat.Builder(context);
+        notifBuilder.setContentText("Temperature in Tehran : ");
+        notifBuilder.setSmallIcon(R.drawable.ic_launcher_background);
+        try {
+
+
+            new FetchData(City.TEHRAN, new UpdateCity() {
+                @Override
+                public void setContent(JsonHandler jsonHandler, int n, boolean isMain) {
+                    temp = jsonHandler.getTemp();
+                    System.out.println(temp);
+                    mainFeel = jsonHandler.getMainWeather();
+                    System.out.println(mainFeel);
+
+                    notifBuilder.setSubText("main feel : "+ mainFeel + "||"+ "temperature: " + String.valueOf(temp));
+                    NotificationManager notificationManager =
+                            (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+                    notificationManager.notify(43211, notifBuilder.build());
+
+                }
+            }, 1).execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+            }
 
     void updateUI() {
         // No internet connection, update the ui and warn the user
